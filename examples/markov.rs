e433ef4fff23c53a55917554f4307f3a32a5ecbf
Leo Conrad-Shah <lconrads@ucsc.edu>
2024-03-22 18:28:54 -0700

    new markov

--- examples/markov.rs
+++ examples/markov.rs
@@ -1,26 +1,32 @@
 use std::{
-    fs::read_dir,
+    fs::{metadata, read_dir},
     io::{stdin, BufRead},
+    os::unix::fs::MetadataExt,
 };
 
 use markov::Chain;
 
 // This is gitignored out because it's massive.
-const DIRECTORY: &str = "examples/dump";
+const DIRECTORY: &str = "../examples/dump";
 
-const MAX_FILES: usize = 100;
+const NUM_FILES: usize = 10_000;
 const GEN_FROM_STDIN: bool = false;
 
 fn main() {
     let training_data: Vec<_> = read_dir(DIRECTORY)
         .unwrap()
-        .take(MAX_FILES)
-        .map(|dir| dir.unwrap().path())
+        .take(NUM_FILES)
+        .filter_map(|dir| {
+            let path = dir.unwrap().path();
+            (metadata(&path).unwrap().size() < 20_000).then(|| path)
+        })
         .collect();
+
+    println!("{}", training_data.len());
 
     let mut chain = Chain::new();
 
-    for path in training_data {
+    for path in std::iter::repeat(&training_data).flatten().take(NUM_FILES) {
         // technically assumes the file is
         // formatted a certain way, but idc
         chain.feed_file(path).unwrap();
