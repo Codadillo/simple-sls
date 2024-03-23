4ffcf6419111613b036ed89975025969abb95f40
Leo Conrad-Shah <lconrads@ucsc.edu>
2024-03-22 17:28:00 -0700

    change markov example

--- examples/markov.rs
+++ examples/markov.rs
@@ -1,7 +1,6 @@
 use std::{
     fs::read_dir,
     io::{stdin, BufRead},
-    time::{Duration, Instant},
 };
 
 use markov::Chain;
@@ -9,29 +8,22 @@ use markov::Chain;
 // This is gitignored out because it's massive.
 const DIRECTORY: &str = "examples/dump";
 
-const MAX_TRAIN_TIME: Duration = Duration::from_secs(60 * 5); 
+const MAX_FILES: usize = 100;
 const GEN_FROM_STDIN: bool = false;
 
 fn main() {
+    let training_data: Vec<_> = read_dir(DIRECTORY)
+        .unwrap()
+        .take(MAX_FILES)
+        .map(|dir| dir.unwrap().path())
+        .collect();
+
     let mut chain = Chain::new();
 
-    let training_start = Instant::now();
-    let dir = read_dir(DIRECTORY).unwrap();
-    for dir_entry in dir {
-        let path = dir_entry.unwrap().path();
-
-        println!("Training chain with file {path:?}");
-        let start = Instant::now();
-
+    for path in training_data {
         // technically assumes the file is
         // formatted a certain way, but idc
         chain.feed_file(path).unwrap();
-
-        println!("Trained in {:?}", start.elapsed());
-
-        if training_start.elapsed() >= MAX_TRAIN_TIME {
-            break;
-        }
     }
 
     if !GEN_FROM_STDIN {
